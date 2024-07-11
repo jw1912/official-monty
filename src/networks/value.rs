61498b5726e7c4b1a891b0b58e754f0cdee075f0
jw1912 <jamie.whiting.dev@gmail.com>
2024-07-11 10:39:50 +0100

    Replaced oldest data with new data Bench: 2209775

--- src/networks/value.rs
+++ src/networks/value.rs
@@ -2,7 +2,7 @@ use crate::Board;
 
 // DO NOT MOVE
 #[allow(non_upper_case_globals)]
-pub const ValueFileDefaultName: &str = "nn-031bf2c50080.network";
+pub const ValueFileDefaultName: &str = "nn-c9d46ff2fcbe.network";
 
 const SCALE: i32 = 400;
 
