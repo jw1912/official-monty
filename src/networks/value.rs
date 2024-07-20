54c4d713af04e0dfcaa3b2fcd9770fc4da1826f2
jw1912 <jamie.whiting.dev@gmail.com>
2024-07-20 14:41:14 +0100

    Bench: 3027591

--- src/networks/value.rs
+++ src/networks/value.rs
@@ -2,7 +2,7 @@ use crate::Board;
 
 // DO NOT MOVE
 #[allow(non_upper_case_globals)]
-pub const ValueFileDefaultName: &str = "nn-12462aca05d0.network";
+pub const ValueFileDefaultName: &str = "nn-7d5e56bf5a67.network";
 
 const SCALE: i32 = 400;
 
