ac66c63154a18d250252f163d726d8a0d6948cf7
jw1912 <jamie.whiting.dev@gmail.com>
2024-08-05 11:46:47 +0100

    Test Policy Arch Baseline Bench: 1243917

--- src/networks/policy.rs
+++ src/networks/policy.rs
@@ -4,20 +4,18 @@ use goober::{activation, layer, FeedForwardNetwork, Matrix, SparseVector, Vector
 
 // DO NOT MOVE
 #[allow(non_upper_case_globals)]
-pub const PolicyFileDefaultName: &str = "nn-6b5dc1d7fff9.network";
+pub const PolicyFileDefaultName: &str = "nn-ba0f5b196f0f.network";
 
 #[repr(C)]
 #[derive(Clone, Copy, FeedForwardNetwork)]
 pub struct SubNet {
     ft: layer::SparseConnected<activation::ReLU, 768, 16>,
-    l2: layer::DenseConnected<activation::ReLU, 16, 16>,
 }
 
 impl SubNet {
     pub const fn zeroed() -> Self {
         Self {
             ft: layer::SparseConnected::zeroed(),
-            l2: layer::DenseConnected::zeroed(),
         }
     }
 
@@ -25,12 +23,8 @@ impl SubNet {
         let matrix = Matrix::from_fn(|_, _| f());
         let vector = Vector::from_fn(|_| f());
 
-        let matrix2 = Matrix::from_fn(|_, _| f());
-        let vector2 = Vector::from_fn(|_| f());
-
         Self {
             ft: layer::SparseConnected::from_raw(matrix, vector),
-            l2: layer::DenseConnected::from_raw(matrix2, vector2),
         }
     }
 }
@@ -38,28 +32,27 @@ impl SubNet {
 #[repr(C)]
 #[derive(Clone, Copy)]
 pub struct PolicyNetwork {
-    pub subnets: [[SubNet; 2]; 448],
+    pub subnets: [[SubNet; 2]; 128],
     pub hce: layer::DenseConnected<activation::Identity, 4, 1>,
 }
 
 impl PolicyNetwork {
     pub const fn zeroed() -> Self {
         Self {
-            subnets: [[SubNet::zeroed(); 2]; 448],
+            subnets: [[SubNet::zeroed(); 2]; 128],
             hce: layer::DenseConnected::zeroed(),
         }
     }
 
     pub fn get(&self, pos: &Board, mov: &Move, feats: &SparseVector, threats: u64) -> f32 {
         let flip = pos.flip_val();
-        let pc = pos.get_pc(1 << mov.src()) - 1;
 
         let from_threat = usize::from(threats & (1 << mov.src()) > 0);
         let from_subnet = &self.subnets[usize::from(mov.src() ^ flip)][from_threat];
         let from_vec = from_subnet.out(feats);
 
         let good_see = usize::from(pos.see(mov, -108));
-        let to_subnet = &self.subnets[64 * pc + usize::from(mov.to() ^ flip)][good_see];
+        let to_subnet = &self.subnets[usize::from(mov.to() ^ flip)][good_see];
         let to_vec = to_subnet.out(feats);
 
         let hce = self.hce.out(&Self::get_hce_feats(pos, mov))[0];
