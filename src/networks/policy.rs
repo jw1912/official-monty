1fd715d937891db700d4b9fd4a87d06c0e80a31a
jw1912 <jamie.whiting.dev@gmail.com>
2024-08-06 14:52:52 +0100

    Bench: 1923750

--- src/networks/policy.rs
+++ src/networks/policy.rs
@@ -4,18 +4,20 @@ use goober::{activation, layer, FeedForwardNetwork, Matrix, SparseVector, Vector
 
 // DO NOT MOVE
 #[allow(non_upper_case_globals)]
-pub const PolicyFileDefaultName: &str = "nn-a7af95e30ed6.network";
+pub const PolicyFileDefaultName: &str = "nn-f9980dfccbb2.network";
 
 #[repr(C)]
 #[derive(Clone, Copy, FeedForwardNetwork)]
 pub struct SubNet {
     ft: layer::SparseConnected<activation::ReLU, 768, 16>,
+    l2: layer::DenseConnected<activation::ReLU, 16, 16>,
 }
 
 impl SubNet {
     pub const fn zeroed() -> Self {
         Self {
             ft: layer::SparseConnected::zeroed(),
+            l2: layer::DenseConnected::zeroed(),
         }
     }
 
@@ -23,8 +25,12 @@ impl SubNet {
         let matrix = Matrix::from_fn(|_, _| f());
         let vector = Vector::from_fn(|_| f());
 
+        let matrix2 = Matrix::from_fn(|_, _| f());
+        let vector2 = Vector::from_fn(|_| f());
+
         Self {
             ft: layer::SparseConnected::from_raw(matrix, vector),
+            l2: layer::DenseConnected::from_raw(matrix2, vector2),
         }
     }
 }
