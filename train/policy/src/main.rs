bea1f1b09646a4c709618dd78372cf0b23f33a01
jw1912 <jamie.whiting.dev@gmail.com>
2024-08-12 12:41:49 +0100

    Bench: 538198

--- train/policy/src/main.rs
+++ train/policy/src/main.rs
@@ -5,5 +5,5 @@ fn main() {
     args.next();
     let threads = args.next().unwrap().parse().unwrap();
 
-    policy::train::<PolicyNetwork>(threads, "data/chess/policy5k.data".to_string(), 60, 25);
+    policy::train::<PolicyNetwork>(threads, "policy.data".to_string(), 60, 25);
 }
