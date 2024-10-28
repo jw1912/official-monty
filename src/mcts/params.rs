1b9527f1c5c811608ff469991087f781115c74c9
jw1912 <jamie.whiting.dev@gmail.com>
2024-10-28 11:58:46 +0000

    Take 3 Bench: 1267333

--- src/mcts/params.rs
+++ src/mcts/params.rs
@@ -145,8 +145,8 @@ make_mcts_params! {
     material_offset: i32 = 700, 400, 1200, 40, 0.002;
     material_div1: i32 = 32, 16, 64, 3, 0.002;
     material_div2: i32 = 1024, 512, 1536, 64, 0.002;
-    kurtosis_offset: f32 = 0.5, 0.0, 2.0, 0.1, 0.002;
-    kurtosis_scale: f32 = 0.1, 0.0, 1.0, 0.05, 0.002;
+    kurtosis_offset: f32 = 1.0, 0.0, 2.0, 0.1, 0.002;
+    kurtosis_scale: f32 = 0.05, 0.0, 1.0, 0.05, 0.002;
 
     // TM constants
     tm_opt_value1: f64 = 0.686, 0.1, 1.2, 0.072, 0.002;
