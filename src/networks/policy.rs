4791e49b7a73d34d3c93945e251fb3513c2d6bcf
jw1912 <jamie.whiting.dev@gmail.com>
2025-05-08 21:29:36 +0100

    Without threat inputs

--- src/networks/policy.rs
+++ src/networks/policy.rs
@@ -1,7 +1,4 @@
-use crate::{
-    boxed_and_zeroed,
-    chess::{Attacks, Board, Move},
-};
+use crate::chess::{Attacks, Board, Move};
 
 use super::{
     accumulator::Accumulator,
@@ -23,7 +20,7 @@ pub const L1: usize = 12288;
 #[repr(C)]
 #[derive(Clone, Copy)]
 pub struct PolicyNetwork {
-    l1: Layer<i8, { 768 * 4 }, L1>,
+    l1: Layer<i8, 768, L1>,
     l2: TransposedLayer<i8, { L1 / 2 }, { 1880 * 2 }>,
 }
 
@@ -111,21 +108,3 @@ const OFFSETS: [usize; 65] = {
 
     offsets
 };
-
-#[repr(C)]
-pub struct UnquantisedPolicyNetwork {
-    l1: Layer<f32, { 768 * 4 }, L1>,
-    l2: Layer<f32, { L1 / 2 }, { 1880 * 2 }>,
-}
-
-impl UnquantisedPolicyNetwork {
-    pub fn quantise(&self) -> Box<PolicyNetwork> {
-        let mut quantised: Box<PolicyNetwork> = unsafe { boxed_and_zeroed() };
-
-        self.l1.quantise_into_i8(&mut quantised.l1, QA, 0.99);
-        self.l2
-            .quantise_transpose_into_i8(&mut quantised.l2, QB, 0.99);
-
-        quantised
-    }
-}
